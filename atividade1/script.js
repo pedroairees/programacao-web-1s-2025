100042b6f1169bb125001e500d6bc3c80ee4493e
pedroaiires <110794196+pedroairees@users.noreply.github.com>
2025-03-02 17:00:00 -0300

    Add files via upload

---
+++ atividade1/script.js
@@ -0,0 +1,8 @@
+let num1 = 5;
+let num2 = 10;
+let operacao = "adição";
+let resultado = num1 + num2;
+
+console.log(`Número 1 --> ${num1}`);
+console.log(`Número 2 --> ${num2}`);
+console.log(`Resultado da ${operacao} entre ${num1} e ${num2} é ${resultado}`);
